1b401adae0afdc658e8e940a4a969169a662a18a
Anders Dahl <dahl.a@husky.neu.edu>
2014-12-29 16:17:20 -0500

    More changes, basically finished

--- website.py
+++ website.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, url_for
 import os
 
 app = Flask(__name__)
+# app.add_url_rule('/favicon.ico',
+#   redirect_to=url_for('static', filename='favicon.ico'))
 
 @app.route("/")
 def index():
   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+  return send_from_directory(os.path.join(app.root_path, 'static'),
+    'favicon.ico',
+    mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 5000))
