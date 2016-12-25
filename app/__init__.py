2a2a987b5f41d42bc0b16b6a0ce841450fe4bde3
Anders Dahl <andersoldahl@gmail.com>
2016-12-24 19:35:35 -0500

    Added ssl, hopefully

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,21 @@
-from flask import Flask
-
+from flask import Flask, abort
+import os
 app = Flask(__name__)
-from app import views
+
+
+def find_key(token):
+    if token == os.environ.get("ACME_TOKEN"):
+        return os.environ.get("ACME_KEY")
+    for k, v in os.environ.items():
+        if v == token and k.startswith("ACME_TOKEN_"):
+            n = k.replace("ACME_TOKEN_", "")
+            return os.environ.get("ACME_KEY_{}".format(n))
+
+
+@app.route('/')
+@app.route("/.well-known/acme-challenge/<token>")
+def acme(token):
+    key = find_key(token)
+    if key is None:
+        abort(404)
+    return key
