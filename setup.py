6152da60ef3b3000f7b4633c8fd6f60633ecede7
Anders Dahl <andersoldahl@gmail.com>
2016-12-26 11:05:26 -0500

    Changed from run to setup, new flask stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!venv/bin/python
+from setuptools import setup
+
+setup(
+    name='andersoldahl',
+    packages=['andersoldahl'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
